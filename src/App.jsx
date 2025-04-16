46433c4519447ee388a535272d8a52a363ce8f83
kamronbekqa <@kamronbekqa!>
2025-04-16 19:20:01 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+      <h1>Welcome to React JS</h1>
+      <h2>THis is my first React app</h2>
+      <ul>
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>JS</li>
+        <li>React</li>
+      </ul>
+    </>
+  );
+}
+
+export default App
