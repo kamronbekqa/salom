a7035f6a728deac4476afc7c9d98afed7841b0cf
kamronbekqa <@kamronbekqa!>
2025-04-18 20:04:00 +0500

    salom dunyo

--- src/App.jsx
+++ src/App.jsx
@@ -1,57 +1,25 @@
 import "./App.css";
 
 function App() {
+  const userName = prompt("Ismingizni kiriting:");
+  const hour = new Date().getHours();
+  const date = new Date().toLocaleDateString();
+  const randomNumber = Math.floor(Math.random() * 101);
+  const isDay = hour >= 6 && hour < 18;
+
   return (
-    <>
-      <header>
-        <div class="container">
-          <div className="header-wrapper">
-            <div className="header-left">
-              <img src="../1.png" alt="" />
-            </div>
-            <div className="header-right">
-              <h3>Home</h3>
-              <h3>Features</h3>
-              <h3>Community</h3>
-              <h3>Blog</h3>
-              <h3>Pricing</h3>
-              <button>Register Now</button>
-            </div>
-          </div>
-        </div>
-      </header>
-      <main>
-        <section class="hero">
-          <div class="container">
-            <div className="hero-wrapper">
-              <div className="hero-left">
-                <h1>
-                  Lessons and insights
-                  <br /> from 8 years
-                </h1>
-                <p>
-                  Where to grow your business as a photographer: site or social
-                  media?
-                </p>
-                <button>Register</button>
-              </div>
-              <div className="hero-right">
-                <img src="../2.png" alt="" />
-              </div>
-            </div>
-          </div>
-        </section>
-      </main>
-      <img className="img" src="../3.png" alt="" /> <br />
-      <br />
-      <img className="img" src="../4.png" alt="" />
-      <br />
-      <br />
-      <img className="img" src="../5.png" alt="" />
-      <footer>
-        <div class="container"></div>
-      </footer>
-    </>
+    <div className={isDay ? "light" : "dark"}>
+      <div className="card">
+        <p>
+          Hello my name is: <span className="highlight">{userName}</span>
+        </p>
+        <p>
+          This is the day: <span className="highlight">{date}</span>
+        </p>
+        <p>A random number from 0 to 100:</p>
+        <h1>{randomNumber}</h1>
+      </div>
+    </div>
   );
 }
 
